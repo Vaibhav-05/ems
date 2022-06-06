2a16c40ed86122f4f4fa6fd1ab38603cc776ebd6
Vaibhav Sahu <vsahu@innominds.com>
2022-06-06 13:06:52 +0530

    initial commit user entity added

---
+++ ems/src/main/java/com/ems/entities/User.java
@@ -0,0 +1,97 @@
+package com.ems.entities;
+
+public class User {
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String contactNo;
+	private String gender;
+	private boolean isAdmin;
+
+	public User(int userId, String firstName, String lastName, String email, String password, String contactNo,
+			String gender, boolean isAdmin) {
+		super();
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.contactNo = contactNo;
+		this.gender = gender;
+		this.isAdmin = isAdmin;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", contactNo=" + contactNo + ", gender=" + gender + ", isAdmin=" + isAdmin
+				+ "]";
+	}
+
+}
